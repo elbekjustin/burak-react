2f012d24c1e667965292a0dfd0070d9d0bc97af5
elbekjustin <elbekjumanazarov03@gmail.com>
2024-10-04 19:19:49 +0900

    ix: MUI cutomized integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 import "../css/app.css";
 import { Box, Container, Stack, Typography, Button } from "@mui/material";
+import { RippleBadge } from "./styled";
 
 function App() {
   return (
-    <Container maxWidth="sm">
+    // <Container maxWidth="sm">
+    <Container sx={{ background: "orange" }}>
       <Stack flexDirection={"column"}>
         <Box sx={{ my: 4}}>
            <Typography variant="h4" component={"h4"}>
             Create React App on TypeScript width REDUX
            </Typography>
         </Box>
-        <Button variant="contained">Contained</Button>
+        <Box>
+          <RippleBadge badgeContent={4}>
+             <Button variant="contained">Contained</Button>
+          </RippleBadge>
+        </Box>
+       
       </Stack>
     </Container>
   );
